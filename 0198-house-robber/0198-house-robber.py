33599e502060d42abff49033613ce7f01fde2576
Siddharth Singh <112953828+Siddharth-Singh-2004@users.noreply.github.com>
2024-01-22 02:16:41 +0530

    Time: 14 ms (71.61%), Space: 11.7 MB (98.64%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def rob(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        rob, norob = 0, 0
+        for num in nums:
+            newRob = norob + num
+            newNoRob = max(norob, rob)
+            rob, norob = newRob, newNoRob
+        return max(rob, norob)
+        
+        
+        
